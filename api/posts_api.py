50534cc3911a95c4c582d1eb5d280967ec47bb01
nitin <nitin.choudhary.057@gmail.com>
2026-07-31 02:53:07 +0530

    added utils

--- api/posts_api.py
+++ api/posts_api.py
@@ -2,9 +2,12 @@ import requests
 from requests import Response
 
 from config.settings import BASE_URL
+from utils.logger import get_logger
 
 
 class PostsAPI:
+
+    logger = get_logger(__name__)
 
     POSTS_ENDPOINT = "/posts"
 
@@ -19,12 +22,14 @@ class PostsAPI:
         return self.session.post(f"{BASE_URL}{path}",json=payload)
 
     def get_all_posts(self) -> Response:
+        self.logger.info("Fetching all posts")
         return self._get(self.POSTS_ENDPOINT)
-
-
+    
     def get_post_by_id(self, post_id: int) -> Response:
+        self.logger.info(f"Fetching post with id {post_id}")
         return self._get(f"{self.POSTS_ENDPOINT}/{post_id}")
 
 
     def create_post(self, payload: dict) -> Response:
+        self.logger.info("Creating a new post")
         return self._post(self.POSTS_ENDPOINT, payload)
